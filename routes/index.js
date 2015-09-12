1b8ad709b4d5702c30bda47fd72bd6ac1dc62a82
Jon Jekeli <jon.jekeli@gmail.com>
2015-09-12 11:11:25 -0400

    Added more pages to website, used ng-dialog as pop-up.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+//MAYBE we could turn this into a SPA, so we only need to render a single page,
+//thus reducing these calls that are specific to web navigation, and allowing the
+//server to be pretty agnostic to the platform accessing it
+//However, not sure if the app is too large to be SPA
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Better My City' });
+  res.render('pages/index');
 });
 
-var cities = require('../models/City.js');
+/* GET who-we-are page. */
+router.get('/who-we-are', function(req, res, next) {
+  res.render('pages/who-we-are');
+});
 
+/* GET find-my-city page. */
+router.get('/find-my-city', function(req, res, next) {
+  res.render('pages/find-my-city');
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('pages/contact');
+});
+
+//CRUD below
 /* GET available cities */
+var cities = require('../models/City.js');
 router.get('/cities', function(req, res, next) {
   cities.find(function (err, result) {
       if (err) {
